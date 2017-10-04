e080311a19d9dae0852767c238352e52916bcfab
Franzé Jr <franzejr@gmail.com>
2017-10-04 13:40:47 -0300

    Set up GA

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,7 @@
 import ReactGA from "react-ga";
 ReactGA.initialize("UA-64071383-7");
 
-exports.onRouteUpdate = (state, page, pages) => {
-  const { pathname } = state;
-  ReactGA.set({ page: pathname });
-  ReactGA.pageview(pathname);
+exports.onClientEntry = () => {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
 };
